14584c158995f24a51cdf49f399e60bc63a11bcd
Ced <cbourneville@gmail.com>
2022-03-29 20:05:56 +1300

    Start setting up EF

---
+++ PuzzleTimer/PuzzleTimer/Models/Puzzles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleTimer.Models
+{
+    public class Puzzles
+    {
+        public string SKU { get; set; }
+        public string Name { get; set; }
+    }
+}
